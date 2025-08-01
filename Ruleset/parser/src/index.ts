a708585375debc93046707a02fa5327eff7b962d
dearealeo <183638396+dearealeo@users.noreply.github.com>
2025-08-02 01:16:46 +0800

    feat: submodules

--- Ruleset/parser/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * as surge from './surge';
-export * as clash from './clash';
-export * as ss from './ss';
-export * as trojan from './trojan';
-export * as vmess from './vmess';
-
-export type * from './types';
